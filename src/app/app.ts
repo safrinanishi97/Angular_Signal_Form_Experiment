aa268169a8f1ca1c34b95182f551fccb1af36d3e
safrina.akter <safrina.akter@hana-ltd.com>
2026-02-18 10:38:13 +0600

    Initial commit - Angular Signal Form Login Project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { form, FormField } from '@angular/forms/signals';
+
+interface LoginData {
+  userName: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [FormField]
 })
 export class App {
   protected readonly title = signal('signal-forms-demo');
+
+  protected readonly loginData = signal<LoginData>({
+    userName: '',
+    email: '',
+    password: ''
+  });
+
+  protected readonly loginForm = form(this.loginData);
+
+  saveLoginForm(){
+    
+  }
 }
